70c98fa03662b40331e5fc9ac1912cac5d4ce171
Sai manas <154975843+saimanasgoud@users.noreply.github.com>
2024-05-04 17:28:47 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+function toggleLinks() {
+    var linksDiv = document.getElementById("additional-links");
+    if (linksDiv.style.display === "none"){
+        linksDiv.style.display = "block";
+    } else {
+        linksDiv.style.display = "none";
+    }
+}
+
